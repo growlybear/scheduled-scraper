218e61db85acc0bc403ee4b0ea280058eb6c2e62
Michael Allan <michaelallan@optushome.com.au>
2014-10-24 16:51:47 +1100

    Exit process on completion

--- models/db.js
+++ models/db.js
@@ -3,10 +3,6 @@ var dbURI = process.env.MONGOHQ_URL;
 
 mongoose.connect(dbURI);
 
-mongoose.connection.on('connected', function () {
-    console.log('Mongoose connected to ' + dbURI);
-});
-
 mongoose.connection.on('error', function (err) {
     console.log('Mongoose connection error:', err);
 });
