fa016f09ff577d74ef0a01cd885b3665c34e49bd
Michael Allan <michaelallan@optushome.com.au>
2015-09-16 05:31:47 -1000

    Reformat

--- models/db.js
+++ models/db.js
@@ -4,17 +4,17 @@ var dbURI = process.env.MONGOHQ_URL;
 mongoose.connect(dbURI);
 
 mongoose.connection.on('error', function (err) {
-    console.log('Mongoose connection error:', err);
+  console.log('Mongoose connection error:', err);
 });
 
 mongoose.connection.on('disconnected', function () {
-    console.log('Mongoose disconnected');
+  console.log('Mongoose disconnected');
 });
 
 // NOTE avoid leaking db connections in case of application errors
 process.on('SIGINT', function () {
-    mongoose.connection.close(function () {
-        console.log('Mongoose disconnected through app termination');
-        process.exit(0);
-    });
+  mongoose.connection.close(function () {
+    console.log('Mongoose disconnected through app termination');
+    process.exit(0);
+  });
 });
